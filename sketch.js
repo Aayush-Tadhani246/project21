c6f770f2ae7ed30b8cf7023a59f3c672a1c9f2b5
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2020-09-18 19:10:42 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var wall,thickness,bullet,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(233,321);
+  thickness=random(22,83);
+  weight=random(30,52) ;
+  bullet = createSprite(50,200,50,10);
+  wall = createSprite(1500,200,thickness,height/2);
+}
+
+function draw() {
+  background(0,0,0);   
+  bullet.velocityX = speed;
+
+  hasCollided(bullet,wall);
+
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+
+    var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+  
+    if (damage>10){
+      wall.shapeColor=color(255,0,0);
+    }
+
+    if (damage<10){
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+
+    drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+
+  bulletRightEdge=lbullet.x + lbullet.width;
+  wallLeftEdge=lwall.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+}
